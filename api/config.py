72e75dc2b484bb5568f5169b4e241a64c21c720f
flydancin <86071988w@gmail.com>
2026-06-08 18:33:25 +0800

    Update config.py

--- api/config.py
+++ api/config.py
@@ -12,7 +12,7 @@ ALLOWED_USERS = split(r'[ ,;，；]+', os.getenv("ALLOWED_USERS", '').replace("@
 ALLOWED_GROUPS = [g.strip() for g in split(r'[,;，；]+', os.getenv("ALLOWED_GROUPS", '')) if g.strip()]
 
 SYSTEM_INSTRUCTION = os.getenv("SYSTEM_INSTRUCTION", "")
-DEFAULT_MODEL = os.getenv("GEMINI_MODEL", "gemini-2.5-flash")
+DEFAULT_MODEL = os.getenv("GEMINI_MODEL", "gemini-3.5-flash")
 
 # After setting up 3 rounds of dialogue, prompt the user to start a new dialogue
 prompt_new_threshold = int(3)
